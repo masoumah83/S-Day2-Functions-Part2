63949f772c16fdf648ebdc85489d2455e196f4c0
masoumah ashknani <m.ashknani@hotmail.com>
2024-08-06 15:30:55 +0300

    done

---
+++ function2.js
@@ -0,0 +1,32 @@
+// task 1//
+
+function isOdd(num) {
+  if (num % 2 != 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let num = isOdd(12);
+console.log(num);
+//console.log(isOdd(12));
+//---------------------------------------------
+
+// task3//
+
+function squareOrDouble(num) {
+  if (num % 2 == 0) {
+    return num * 2;
+  } else {
+    return num * num;
+  }
+}
+
+console.log(squareOrDouble(9));
+
+//---------------------------------------------
+// task 2
+
+//function oddsSmallerThan(num) {}
+
+//alert(oddsSmallerThan(10));
